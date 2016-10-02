1292e4f0cc253fbb91d9a836c220f2630b4e10aa
oopsjpeg <oopsjpeg@gmail.com>
2016-10-02 09:41:42 +0900

    Added ReadyEvent to EventHandler

--- discord/src/main/java/com/aegeus/discord/EventHandler.java
+++ discord/src/main/java/com/aegeus/discord/EventHandler.java
@@ -2,11 +2,17 @@ package com.aegeus.discord;
 
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+import sx.blah.discord.handle.impl.events.ReadyEvent;
 
 public class EventHandler {
 	
 	@EventSubscriber
-	public void onMessage(MessageReceivedEvent e)	{
+	public void onReady(ReadyEvent event) {
+		System.out.println("CORIUS initialized.");
+	}
+	
+	@EventSubscriber
+	public void onMessage(MessageReceivedEvent event) {
 		
 	}
 }
