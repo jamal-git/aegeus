540678f5566301a872f1aa4fd940a3edcf5b9003
Crafty <jvwill00@gmail.com>
2017-07-11 07:52:35 -0400

    Added mask check to stats listener (WIP)
    Automatically changes pumpkins to jack-o-lanterns when equipped. WIP

--- game/src/main/java/com/aegeus/game/listener/StatsListener.java
+++ game/src/main/java/com/aegeus/game/listener/StatsListener.java
@@ -4,10 +4,14 @@ import com.aegeus.game.Aegeus;
 import com.aegeus.game.entity.AgPlayer;
 import com.aegeus.game.util.Util;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class StatsListener implements Listener {
 	private final Aegeus parent;
@@ -43,7 +47,27 @@ public class StatsListener implements Listener {
 
 	@EventHandler
 	private void onInventoryClick(InventoryClickEvent e) {
-		Bukkit.getScheduler().runTaskLater(parent, () -> Util.updateStats(e.getWhoClicked()), 1);
+		Bukkit.getScheduler().runTaskLater(parent, () -> {
+			Util.updateStats(e.getWhoClicked());
+			ItemStack item = e.getCurrentItem();
+
+			if (item != null) {
+				if (item.getType().equals(Material.PUMPKIN)
+						&& e.getSlotType().equals(InventoryType.SlotType.ARMOR))
+					item.setType(Material.JACK_O_LANTERN);
+				else if (item.getType().equals(Material.JACK_O_LANTERN)
+						&& !e.getSlotType().equals(InventoryType.SlotType.ARMOR))
+					item.setType(Material.PUMPKIN);
+			}
+
+		}, 1);
+	}
+
+	@EventHandler
+	private void onDrop(PlayerDropItemEvent e) {
+		ItemStack item = e.getItemDrop().getItemStack();
+		if (item != null && item.getType().equals(Material.JACK_O_LANTERN))
+			item.setType(Material.PUMPKIN);
 	}
 
 }
