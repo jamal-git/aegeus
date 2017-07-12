f5e28a4586577e6356e7146b9fc9acea3d8a6cab
Silvre <silvre76@gmail.com>
2017-07-12 13:23:11 -0500

    Console users can no longer generate dungeons
    Since dungeons will need to be placed at a location, they cannot be generated by
    the console since the console has no location in the game.
    Added some catch blocks for CommandCreateDungeon

--- game/src/main/java/com/aegeus/game/commands/world/CommandCreateDungeon.java
+++ game/src/main/java/com/aegeus/game/commands/world/CommandCreateDungeon.java
@@ -3,9 +3,13 @@ package com.aegeus.game.commands.world;
 import com.aegeus.game.dungeon.Dungeon;
 import com.aegeus.game.util.Util;
 import com.aegeus.game.util.exceptions.DungeonLoadingException;
+import com.sk89q.worldedit.data.DataException;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+
+import java.io.IOException;
 
 /**
  * Created by Silvre on 7/11/2017.
@@ -17,10 +21,10 @@ public class CommandCreateDungeon implements CommandExecutor {
             sender.sendMessage(Util.colorCodes("&cYou do not have sufficient permissions to execute this command!"));
             return false;
         }
-//        if(sender instanceof ConsoleCommandSender)  {
-//            sender.sendMessage(Util.colorCodes("&cYou can't execute this command through the console! Get into the game!"));
-//            return false;
-//        }
+        if(sender instanceof ConsoleCommandSender)  {
+            sender.sendMessage(Util.colorCodes("&cYou can't execute this command through the console! Get into the game!"));
+            return false;
+        }
         if(args.length != 2)    {
             sender.sendMessage(Util.colorCodes("&cInvalid arguments! Use /dungeon <directory> <length>"));
             return false;
@@ -28,6 +32,10 @@ public class CommandCreateDungeon implements CommandExecutor {
         try {
             Dungeon d = new Dungeon(args[0], Integer.valueOf(args[1]));
         } catch (DungeonLoadingException e) {
+            e.printStackTrace();
+        } catch (DataException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return true;
