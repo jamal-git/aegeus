5b044cec35c2b5c4f18a5bc819924f8e412c15e5
Silvre <silvre76@gmail.com>
2017-09-12 15:41:30 -0500

    Re-added in glowing enchant to sun and moon ingot
    i

--- game/src/main/java/com/aegeus/game/item/EnumCraftingMaterial.java
+++ game/src/main/java/com/aegeus/game/item/EnumCraftingMaterial.java
@@ -2,6 +2,8 @@ package com.aegeus.game.item;
 
 import com.aegeus.game.util.Util;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -23,40 +25,40 @@ public enum EnumCraftingMaterial {
             Material.PAPER),
     SUN_INGOT("Sun Ingot", EnumMaterialRarity.MYTHICAL,
             Util.colorCodes("&7The unmeasurable amount of power of the sun in a tangible form.", "&7This fabled ingot can be used in crafting very rare items."),
-            Material.GOLD_INGOT),//e {
-//        @Override
-//        public ItemStack getItem() {
-//            ItemStack stack = new ItemStack(getMaterial(), 1);
-//            ItemMeta meta = stack.getItemMeta();
-//            meta.setDisplayName(Util.colorCodes("&eSun Ingot"));
-//            List<String> lore = new ArrayList<>();
-//            lore.addAll(getDescription());
-//            lore.add(getRarity().getNameAndColor());
-//            meta.setLore(lore);
-//            meta.addEnchant(Enchantment.ARROW_INFINITE, 1, false);
-//            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-//            stack.setItemMeta(meta);
-//            return stack;
-//        }
-//    },
+            Material.GOLD_INGOT) {
+        @Override
+        public ItemStack getItem() {
+            ItemStack stack = new ItemStack(getMaterial(), 1);
+            ItemMeta meta = stack.getItemMeta();
+            meta.setDisplayName(Util.colorCodes("&eSun Ingot"));
+            List<String> lore = new ArrayList<>();
+            lore.addAll(getDescription());
+            lore.add(getRarity().getNameAndColor());
+            meta.setLore(lore);
+            meta.addEnchant(Enchantment.ARROW_INFINITE, 1, false);
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            stack.setItemMeta(meta);
+            return stack;
+        }
+    },
     MOON_INGOT("Moon Ingot", EnumMaterialRarity.MYTHICAL,
             Util.colorCodes("&7The essence of the moon in a tangible form.", "&7This fabled ingot can be used in crafting very rare items."),
-            Material.IRON_INGOT)//{
-//        @Override
-//        public ItemStack getItem() {
-//            ItemStack stack = new ItemStack(getMaterial(), 1);
-//            ItemMeta meta = stack.getItemMeta();
-//            meta.setDisplayName(Util.colorCodes("&bMoon Ingot"));
-//            List<String> lore = new ArrayList<>();
-//            lore.addAll(getDescription());
-//            lore.add(getRarity().getNameAndColor());
-//            meta.setLore(lore);
-//            meta.addEnchant(Enchantment.ARROW_INFINITE, 1, false);
-//            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-//            stack.setItemMeta(meta);
-//            return stack;
-//        }
-    ;
+            Material.IRON_INGOT){
+        @Override
+        public ItemStack getItem() {
+            ItemStack stack = new ItemStack(getMaterial(), 1);
+            ItemMeta meta = stack.getItemMeta();
+            meta.setDisplayName(Util.colorCodes("&bMoon Ingot"));
+            List<String> lore = new ArrayList<>();
+            lore.addAll(getDescription());
+            lore.add(getRarity().getNameAndColor());
+            meta.setLore(lore);
+            meta.addEnchant(Enchantment.ARROW_INFINITE, 1, false);
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            stack.setItemMeta(meta);
+            return stack;
+        }};
+
 
 
     private String name;
